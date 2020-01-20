6a2783e1f6a07db8329182fae6a94f46e8a4c4ca
Diego <diegomugnae@gmail.com>
2020-01-21 08:31:22 +1000

    Modified python engine to flask
    added some flask lines however still need to study more this subject

--- retrieveAPI.py
+++ retrieveAPI.py
@@ -3,6 +3,18 @@
 import urllib.request
 import json
 import textwrap
+#from flask import Flask
+#from flask import request
+
+bookSearch = ""
+
+#have to learn flask first!
+#@app.route('/searchForBook', methods=['POST'])
+#def searchForBook():
+#    if request.method == "POST":
+#        bookSearch = request.form['book']
+#    else:
+#        bookSearch = 'Invalid book'
 
 bookSearch = input("qual livro?")
 #with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:1408865262") as f:
