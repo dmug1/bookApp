55f284d5c3ec75aa0272c01775b34c7955756aea
Diego <diegomugnae@gmail.com>
2020-01-22 12:14:37 +1000

    Attributes returned info to html page

--- egn.py
+++ egn.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, url_for, request
-#from retrieveAPI import *
+from flask import Flask, render_template, url_for, request, redirect
+from retrieveAPI import *
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def index():
+    bookInfo = ""
     if request.method == "POST":
         task_content = request.form['book']
+        bookInfo =  getBookInfo(task_content)      
+        return render_template("bookapp.html", bookInfo=bookInfo)
     else:
-        return render_template("bookapp.html")
+        return render_template("bookapp.html", bookInfo=bookInfo)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- retrieveAPI.py
+++ retrieveAPI.py
@@ -4,39 +4,20 @@ import urllib.request
 import json
 import textwrap
 import sys
-#from flask import Flask
-#from flask import request
 
-bookSearch = ""
+def getBookInfo(bookSearch):
+    bookSearch = bookSearch.replace(" ","%20")
+    urlContainer = "https://www.googleapis.com/books/v1/volumes?q=" + bookSearch
+    print(urlContainer)
+    try:
+        with urllib.request.urlopen(urlContainer) as f:
+            text = f.read()
+            decodedtext = text.decode('utf-8')
+            print(textwrap.fill(decodedtext, width=50))
+    except Exception as ec:
+        print("Connection error, could not reach Google API: %s" %str(ec))
+       
 
-#have to learn flask first!
-#@app.route('/searchForBook', methods=['POST'])
-#def searchForBook():
-#    if request.method == "POST":
-#        bookSearch = request.form['book']
-#    else:
-#        bookSearch = 'Invalid book'
-
-#bookSearch = input("qual livro?")
-#with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:1408865262") as f:
-bookSearch = "Dom Casmurro"
-bookSearch = bookSearch.replace(" ","%20")
-
-urlContainer = "https://www.googleapis.com/books/v1/volumes?q=" + bookSearch
-
-print(urlContainer)
-
-#with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=harry%20potter") as f:
-try:
-    with urllib.request.urlopen(urlContainer) as f:
-        text = f.read()
-        decodedtext = text.decode('utf-8')
-        print(textwrap.fill(decodedtext, width=50))
-except Exception as ec:
-    print("Connection error, could not reach Google API: %s" %str(ec))
-
-    
-print()
-
-obj = json.loads(decodedtext)
-print(obj['items'][0]['searchInfo']['textSnippet'])
+    obj = json.loads(decodedtext)
+    print(obj['items'][0]['searchInfo']['textSnippet'])
+    return(obj['items'][0]['searchInfo']['textSnippet'])
